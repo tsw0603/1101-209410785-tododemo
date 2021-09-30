e4c21ee717ad9be1709e60540596e1d1111d5ee4
tsw0603 <209410785@gms.tku.edu.tw>
2021-09-30 20:27:44 +0800

    first commit

---
+++ src/App_85.js
@@ -0,0 +1,15 @@
+import Todo_85 from './components/Todo_85';
+import './App.css';
+
+function App_85() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo_85 text='Learn React' />
+      <Todo_85 text='Learn SQK' />
+      <Todo_85 text='Learn NextJS' />
+    </div>
+  );
+}
+
+export default App_85;
